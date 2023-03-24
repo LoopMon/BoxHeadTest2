2e127ec51531461cc2fe1fa022ab3c9c9f8e6866
João Lucas <introducaoteste@gmail.com>
2023-03-24 03:32:58 -0300

    arquivos .js com as classes do game

---
+++ js/class/player.js
@@ -0,0 +1,29 @@
+class Player {
+    constructor (
+        x = 0,
+        y = 0,
+        width = 30,
+        height = 60,
+        color = "gray",
+        up = "w",
+        down = "s",
+        left = "a",
+        right = "d"
+    ) {
+        // dimensões e cor
+        this.x = x
+        this.y = y
+        this.w = width
+        this.h = height
+        this.c = color
+
+        // movimentos
+        this.btnUp = up
+        this.btnDown = down
+        this.btnLeft = left
+        this.btnRight = right
+        this.mvUp = this.mvDown = this.mvLeft = this.mvRight = false
+        this.speed = 2
+        this.currentDirection = 0
+    }
+}
