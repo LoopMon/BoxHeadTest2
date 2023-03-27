960abf8b20b846893d9c9f1eb429e900ce32e91b
João Lucas <introducaoteste@gmail.com>
2023-03-27 02:21:36 -0300

    Chamando as funcs. drawObj e colisionWithObj, para desenhar e verificar a colisão para cada pilar

--- js/funcs/mainFunctions.js
+++ js/funcs/mainFunctions.js
@@ -1,10 +1,18 @@
 function draw() {
+    drawObj(collumn1, 1)
+    drawObj(collumn2, 1)
+    drawObj(collumn3, 1)
+    drawObj(collumn4, 1)
     drawObj(player)
 }
 
 function update() {
     ctx.clearRect(0, 0, cnv.width, cnv.height)
     moveObj(player)
+    colisionWithObj(player, collumn1)
+    colisionWithObj(player, collumn2)
+    colisionWithObj(player, collumn3)
+    colisionWithObj(player, collumn4)
     colisionWithCanvas(player)
 }
 
