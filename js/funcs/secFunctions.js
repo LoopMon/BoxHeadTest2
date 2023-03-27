144997d9d4e34cfd7498706c83c5733b87ba32df
João Lucas <introducaoteste@gmail.com>
2023-03-27 02:20:12 -0300

    Melhorando a func. drawObj, e criação da func. colisionWithObj

--- js/funcs/secFunctions.js
+++ js/funcs/secFunctions.js
@@ -1,36 +1,51 @@
 function getActionOn(player, action) {
     if (action == player.btnUp) {
         player.mvUp = true
+        player.mv = true
     }
     if (action == player.btnDown) {
         player.mvDown = true
+        player.mv = true
     }
     if (action == player.btnLeft) {
         player.mvLeft = true
+        player.mv = true
     }
     if (action == player.btnRight) {
         player.mvRight = true
+        player.mv = true
     }
 }
 
 function getActionOff(player, action) {
     if (action == player.btnUp) {
         player.mvUp = false
+        player.mv = false
     }
     if (action == player.btnDown) {
         player.mvDown = false
+        player.mv = false
     }
     if (action == player.btnLeft) {
         player.mvLeft = false
+        player.mv = false
     }
     if (action == player.btnRight) {
         player.mvRight = false
+        player.mv = false
     }
 }
 
-function drawObj(player) {
-    ctx.fillStyle = player.c
-    ctx.fillRect(player.x, player.y, player.w, player.h)
+function drawObj(player, type=0) {
+    if (type === 0) {
+        ctx.fillStyle = player.c
+        ctx.fillRect(player.x, player.y, player.w, player.h)
+    }
+    if (type === 1) {
+        ctx.strokeStyle = player.c
+        ctx.lineWidth = 3
+        ctx.strokeRect(player.x, player.y, player.w, player.h)
+    }
 }
 
 function moveObj(player) {
@@ -60,6 +75,26 @@ function colisionWithCanvas(player) {
     }
     if (player.y < 0) {
         player.y = 0
+    }
+}
+
+function colisionWithObj(player, obj) {
+    if (
+        (player.x + player.w > obj.x) && (player.x < obj.x + obj.w) && 
+        (player.y + player.h > obj.y) && (player.y < obj.y + obj.h)
+    ) {
+        if(player.mvUp){
+            player.y += player.speed 
+        }
+        if(player.mvDown){
+            player.y -= player.speed
+        }
+        if(player.mvLeft){
+            player.x += player.speed
+        }
+        if(player.mvRight){
+            player.x -= player.speed
+        }
     }
 }
 
