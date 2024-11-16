22f104bc849ec655ba2700024a45d457ddb68cc3
moonzera <introducaoteste@gmail.com>
2024-11-16 01:13:16 -0300

    Funções para detecção de teclado

---
+++ src/js/funcs/keyBoardEvents.js
@@ -0,0 +1,44 @@
+function getActionOn(player, action) {
+  if (action == player.btnUp) {
+    player.mvUp = true
+    player.mv = true
+  }
+  if (action == player.btnDown) {
+    player.mvDown = true
+    player.mv = true
+  }
+  if (action == player.btnLeft) {
+    player.mvLeft = true
+    player.mv = true
+  }
+  if (action == player.btnRight) {
+    player.mvRight = true
+    player.mv = true
+  }
+}
+
+function getActionOff(player, action) {
+  if (action == player.btnUp) {
+    player.mvUp = false
+    player.mv = false
+  }
+  if (action == player.btnDown) {
+    player.mvDown = false
+    player.mv = false
+  }
+  if (action == player.btnLeft) {
+    player.mvLeft = false
+    player.mv = false
+  }
+  if (action == player.btnRight) {
+    player.mvRight = false
+    player.mv = false
+  }
+}
+
+document.addEventListener("keydown", (e) => {
+  getActionOn(game.player, e.key)
+})
+document.addEventListener("keyup", (e) => {
+  getActionOff(game.player, e.key)
+})
