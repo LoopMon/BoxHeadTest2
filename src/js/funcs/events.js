411df3a1a30399d4033e73d0e04f96ffbdf819d8
moonzera <introducaoteste@gmail.com>
2024-11-16 01:18:10 -0300

    atualizando as importações

--- src/js/funcs/events.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("keydown", (e) => {
-  getActionOn(game.player, e.key)
-})
-document.addEventListener("keyup", (e) => {
-  getActionOff(game.player, e.key)
-})
--- src/js/index.js
+++ src/js/index.js
@@ -4,28 +4,20 @@ const ctx = cnv.getContext("2d")
 
 // Dimensões do canvas
 cnv.width = Math.floor(window.innerWidth / 2)
-cnv.height = Math.floor(window.innerHeight / 2)
+cnv.height = Math.floor(window.innerHeight / 3)
 
 // variáveis do game
 const player = new Player(Math.floor(cnv.width / 2), Math.floor(cnv.height / 2))
-const column1 = new Column(
-  Math.floor(cnv.width / 4),
-  Math.floor(cnv.height / 4)
-)
-const column2 = new Column(
-  Math.floor(cnv.width / 4) * 3,
-  Math.floor(cnv.height / 4)
-)
-const column3 = new Column(
-  Math.floor(cnv.width / 4),
-  Math.floor(cnv.height / 4) * 3
-)
-const column4 = new Column(
-  Math.floor(cnv.width / 4) * 3,
-  Math.floor(cnv.height / 4) * 3
-)
-
-const game = new BoxHead(cnv, ctx, player, [column1, column2, column3, column4])
+const columns = [
+  new Column(Math.floor(cnv.width / 4), Math.floor(cnv.height / 4)),
+  new Column(Math.floor(cnv.width / 4) * 3, Math.floor(cnv.height / 4)),
+  new Column(Math.floor(cnv.width / 4), Math.floor(cnv.height / 4) * 3),
+  new Column(Math.floor(cnv.width / 4) * 3, Math.floor(cnv.height / 4) * 3),
+]
+columns.forEach((column) => {
+  column.centered()
+})
+const game = new BoxHead(cnv, ctx, player, columns)
 
 const estados = {
   pause: 0,
@@ -39,8 +31,5 @@ const directions = {
   left: 4,
   right: 6,
 }
-
-// controle debbug
-let consoles = 0
 
 game.init()
